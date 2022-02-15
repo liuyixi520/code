04041420d7fcf11c6fe21261f2a65623af8393df
liuyixi520 <liuyixi.520@icloud.com>
2022-02-15 14:00:36 +0800

    delete nowcoder floder

--- nowcoder/huaweiTest/HJ1.py
+++
@@ -1,7 +0,0 @@
-import string
-
-
-s = input()
-arr = s.split(' ')
-n = len(arr)
-print(len(arr[n-1]))
--- nowcoder/huaweiTest/HJ2.py
+++
@@ -1,8 +0,0 @@
-s = input()
-c = input()
-s = s.upper()
-c = c.upper()
-count = 0
-for i in s:
-    if(c==i): count += 1
-print(count)
