0f5820d7b059f68fac7bdb3a09ef03b1a06373c4
liuyixi520 <liuyixi.520@icloud.com>
2022-03-22 23:02:18 +0800

    update some note

---
+++ leetcode/102.py
@@ -0,0 +1,11 @@
+# 二叉树层序遍历
+# 每一层的节点有2**(i-1)个
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
