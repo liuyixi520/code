eebf53ad4ca5b87ac24afb11e95929de4f82c6c3
liuyixi520 <liuyixi.520@icloud.com>
2022-04-21 23:23:27 +0800

    update some note

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-# print hello world
-import os
-print("Hello World")
-os.mkdir('test')
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+turtle.mainloop()
