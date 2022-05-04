6f1a98aafc36e468021d46fa0eee022c48fbd4f6
liuyixi520 <liuyixi.520@icloud.com>
2022-05-04 22:00:33 +0800

    update some code

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-# 加载mnist数据集
-# 这里和书里不一样，书里是把数据集先下载下来的
-# 可能是不想用keras这个包，和自己提前说的只用numpy和matplotlib
-from keras.datasets import mnist
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-print(X_train.shape)
-print(y_train.shape)
-print(X_test.shape)
-print(y_test.shape)
-
-# 查看一下数据集第一个样本和标签
-from PIL import Image
-img = Image.fromarray(X_train[0])
-img.show()
+print('hello world')
