d290f8bd09ecfefb1a0569f44bf3d5580ffe1226
liuyixi520 <liuyixi.520@icloud.com>
2022-05-11 19:36:02 +0800

    add drawingByMouth project

--- test.py
+++ test.py
@@ -1,24 +1,19 @@
-# -*- coding: utf-8 -*-
-import eel
-@eel.expose
-def test_print(str):
-    
-    print("进来了",str)
-    test_process(str)  #python 中的处理过程，再调用js中的函数返回
-    
-    
-def test_process(str):
-    str2 = str+'已处理'
-    eel.system_sends_message2(str2)  # 调用js文件里的函数，将结果返回去
+import os
+import openai
 
-def my_add(a, b):
-    return a+b
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def say_hello_py(x):
-    print(f'Hello {x}')
+start_sequence = "\nA:"
+restart_sequence = "\n\nQ: "
 
-# eel.say_hello_js('test')
-
-if __name__ == '__main__':
-    eel.init("/home/y/Documents/code")
-    eel.start("test.html")
+response = openai.Completion.create(
+  engine="text-davinci-002",
+  prompt="I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"Unknown\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: Unknown\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ:",
+  temperature=0,
+  max_tokens=100,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop=["\n"],
+)
+print(response.choices[0].text)
