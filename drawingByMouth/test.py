ce407ca6d36a2ca6170741b7c9c57ceae9ad004e
liuyixi520 <liuyixi.520@icloud.com>
2022-05-12 23:40:17 +0800

    update some code

--- drawingByMouth/test.py
+++ drawingByMouth/test.py
@@ -7,3 +7,4 @@ proxy = {'https': 'localhost:1089'}
 translator = Translator(proxies=None)
 print(f'中译英：{translator.translate(text_zh, dest="en").text}')
 print(f'英译中：{translator.translate(text_en, dest="zh-cn").text}')
+
