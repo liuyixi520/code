5065f0828b5d29d3db054931740185c1f5526cce
liuyixi520 <liuyixi.520@icloud.com>
2022-05-16 17:15:50 +0800

    update something

--- openai/app.py
+++ openai/app.py
@@ -1,35 +1,9 @@
-import os
-
-import openai
-from flask import Flask, redirect, render_template, request, url_for
-
+from flask import Flask
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
-@app.route("/", methods=("GET", "POST"))
+@app.route('/')
 def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+    return '<h1>Hello World!</h1>'
+  
+if __name__ == '__main__':
+  app.run(debug=True)
